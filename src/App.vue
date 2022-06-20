3fc18f5ab6491881a6a41851b47fd1a7adc4008a
carmen279 <uo269689@uniovi.es>
2022-06-20 13:03:12 +0200

    Feature - Implemented nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,14 @@
 <template>
   <div id="app">
     <nav>
-      <router-link to="/">Inicio</router-link> |
-      <router-link to="/info">About</router-link>
+      <router-link to="/"><i class="fa fa-home fa-2x"></i></router-link>
+      <router-link to="/busca"><i class="fa fa-search fa-2x"></i></router-link>
+      <router-link to="/explora"
+        ><i class="fa fa-line-chart fa-2x"></i
+      ></router-link>
+      <router-link to="/info"
+        ><i class="fa fa-info-circle fa-2x"></i
+      ></router-link>
     </nav>
     <router-view />
   </div>
@@ -18,15 +24,43 @@
 }
 
 nav {
-  padding: 30px;
-
+  padding: 20px 20px;
+  display: flex;
+  justify-content: flex-end;
+  position: fixed;
+  right: 0;
   a {
+    margin-right: 10px;
+    padding: 8px 10px;
     font-weight: bold;
     color: #2c3e50;
+    border: solid 5px;
+    border-radius: 30px;
 
     &.router-link-exact-active {
       color: #42b983;
     }
+  }
+
+  i:hover:after {
+    content: "Otro";
+    padding-left: 10px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    font-weight: bold;
+    font-size: 22px;
+  }
+
+  .fa-home:hover:after {
+    content: "Inicio";
+  }
+  .fa-search:hover:after {
+    content: "Busca";
+  }
+  .fa-line-chart:hover:after {
+    content: "Explora";
+  }
+  .fa-info-circle:hover:after {
+    content: "Info";
   }
 }
 </style>
